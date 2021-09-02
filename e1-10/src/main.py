9b28f01e4b1bde5da148d4b8a31f8bba69d2f6d1
Thor <thor@pop-os.localdomain>
2021-09-02 13:35:05 +0100

    e1

--- e1-10/src/main.py
+++ e1-10/src/main.py
@@ -1,17 +1,23 @@
+import time
+
 def timing(f):
-    """Convenient decorator to time a function. . Borrowed from:
+    """Convenient decorator to time a function. Borrowed from:
     https://stackoverflow.com/questions/5478351/python-time-measure-function#5478448"""
     def wrap(*args, **kwargs):
         time1 = time.time()
         ret = f(*args, **kwargs)
         time2 = time.time()
-        print('%s function took %0.3f ms'.format(f.__name__, (time2-time1)*1000.0))
+        print('{:s} function took {:.3f} ms'.format(f.__name__, (time2-time1)*1000.0))
+
         return ret
     return wrap
 
 @timing
 def e1():
-    print("hello thor!")
+    threes = 3 * 333 * 334 / 2
+    fives = 5 * 199 * 200 / 2
+    fifteens = 15 * 66 * 67 / 2
+    print(threes + fives - fifteens)
 
 
 def main():
--- e1-10/src/main.rs
+++ e1-10/src/main.rs
@@ -1,4 +1,10 @@
-fn e1() {}
+fn e1() {
+    let threes = 3 * 333 * 334 / 2;
+    let fives = 5 * 199 * 200 / 2;
+    let fifteens = 15 * 66 * 67 / 2;
+    let out = threes + fives - fifteens;
+    println!("{}", out);
+}
 
 fn main() {
     let now = std::time::Instant::now();
