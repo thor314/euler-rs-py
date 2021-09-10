9472272064b4f4d2bd65fd16cb09bdd352e96259
Thor <thorck@protonmail.com>
2021-09-10 17:28:59 +0100

    e11-12

--- e11-20/src/main.py
+++ e11-20/src/main.py
@@ -5,6 +5,98 @@ import math
 from importlib.machinery import SourceFileLoader
 time = SourceFileLoader("main","../../e1-10/src/main.py").load_module()
 
+# read the 20x20 grid from a file, parse it into a 2d array, search in the 4 directions for the max product
 @time.timing
 def e11():
-    print("hi")
+    from pathlib import Path
+    import os
+    # FileNotFoundError: [Errno 2] No such file or directory: 'e8.txt'
+    if os.path.isfile('/home/thor/euler/e11-20/src/e11.txt'):
+        s = Path('/home/thor/euler/e11-20/src/e11.txt').read_text()
+        #print(s)
+    # parse it (how?)
+    # this took about 10 mins to figure out
+    rows = s.split('\n')
+    v=[]
+    i=0
+    for row in rows:
+        r = [int(k) for k in row.split()]
+        v.insert(i,r)
+        i+=1
+    # now search:
+    # h: v[i,j..j+3], v: v[i..i+3,j], d1: v[i..i+3,j..j+3], d2: v[i..i+3,j..j-3]
+    big=0
+    for i in range(7,20):
+        for j in range(20):
+            if j < 17: # horizontal
+                temp = v[i][j] * v[i][j+1] * v[i][j+2] * v[i][j+3]
+                if temp > big:
+                    big = temp
+                    print("h_ new biggest: %s starting at: (%s,%s), with init value %s:" % (big,i,j, v[i][j]))
+                if i < 17: # d1
+                    temp = v[i][j] * v[i+1][j+1] * v[i+2][j+2] * v[i+3][j+3]
+                    if temp > big:
+                        big = temp
+                        print("d\ new biggest: %s starting at: (%s,%s), with init value %s:" % (big,i,j, v[i][j]))
+
+            if i < 17: # vertical
+                temp = v[i][j] * v[i+1][j] * v[i+2][j] * v[i+3][j]
+                if temp > big:
+                    big = temp
+                    print("v| new biggest: %s starting at: (%s,%s), with init value %s:" % (big,i,j, v[i][j]))
+                if j > 2: # d2
+                    temp = v[i][j] * v[i+1][j-1] * v[i+2][j-2] * v[i+3][j-3]
+                    if temp > big:
+                        big = temp
+                        print("d/ new biggest: %s starting at: (%s,%s), with init value %s:" % (big,i,j, v[i][j]))
+
+    print(big)
+    # 10m to write this part... but oof, wrong answer. debugggggg.
+    # the numbers 51267216 = product 66 91 88 97, do exist vertically in the grid.
+    # edge conditions? My edges seem fine.
+    # not counting up to biggest? Seems not.
+    # oh. I transposed a line in d/. Lame. That took 30m to find.
+
+def multiplicities(inp):
+    d = {}
+    n = inp
+    if n < 2:
+        return d
+    while n%2==0:
+        if not d.get(2):
+            d[2] = 1
+        else:
+            d[2] = d[2]+1
+
+        n /= 2
+        #print("2: %s" % d.get(2))
+    i = 3;
+    while n > 1:
+        while n % i == 0:
+            if not d.get(i):
+                d[i] = 1
+            else:
+                d[i] = d[i]+1
+
+            #print("%s: %s" % (i,d.get(i)))
+            n /= i
+        i+=2
+    #print("%s has multiplicities: %s" %(inp, d))
+    return d
+
+@time.timing
+def e12():
+    import functools
+    i = 2
+    sum = 1
+    while True:
+        sum+=i
+        i+=1
+        divisors = multiplicities(sum)
+        n_divisors = 1
+        f = lambda acc, x: acc * (x+1)
+        [n_divisors := f(n_divisors, x) for x in divisors.values()]
+
+        if n_divisors > 500:
+            print("value: %s, the %sth triangle with %s divisors" %(sum, i, n_divisors))
+            break
--- e11-20/src/main.rs
+++ e11-20/src/main.rs
@@ -1,5 +1,113 @@
+// a procedural macro decorator to do similar stuff to my python decorator
+extern crate timings_proc_macro;
+use timings_proc_macro::timings;
+
+#[timings]
+fn e11() {
+    let s: Vec<usize> = std::fs::read_to_string("src/e11.txt")
+        .unwrap()
+        .split_whitespace()
+        .map(|n| n.parse::<usize>().unwrap())
+        .collect();
+    //println!("{:?}", s);
+    // could just run with s, but let's build our 2d array.
+    let mut v = [[0; 20]; 20];
+    (0..400).for_each(|i| v[i / 20][i % 20] = s[i]);
+    //println!("{:?}", v);
+    let mut big = 0;
+    use itertools::Itertools;
+    (0..20).cartesian_product(0..20).for_each(|(i, j)| {
+        if i < 17 {
+            // h_
+            let temp = v[i][j] * v[i + 1][j] * v[i + 2][j] * v[i + 3][j];
+            if temp > big {
+                // println!(
+                //     "h_ new biggest: {} starting at: ({},{}), with init value {}:",
+                //     big, i, j, v[i][j]
+                // );
+                big = temp
+            }
+        }
+        if j < 17 {
+            // v|
+            let temp = v[i][j] * v[i][j + 1] * v[i][j + 2] * v[i][j + 3];
+            if temp > big {
+                // println!(
+                //     "v| new biggest: {} starting at: ({},{}), with init value {}:",
+                //     big, i, j, v[i][j]
+                // );
+                big = temp
+            }
+        }
+        if i < 17 && j < 17 {
+            // d\
+            let temp = v[i][j] * v[i + 1][j + 1] * v[i + 2][j + 2] * v[i + 3][j + 3];
+            if temp > big {
+                // println!(
+                //     "d\\ new biggest: {} starting at: ({},{}), with init value {}:",
+                //     big, i, j, v[i][j],
+                // );
+                big = temp
+            }
+        }
+        if i < 17 && j > 2 {
+            // d/
+            let temp = v[i][j] * v[i + 1][j - 1] * v[i + 2][j - 2] * v[i + 3][j - 3];
+            if temp > big {
+                // println!(
+                //     "d/ new biggest: {} starting at: ({},{}), with init value {}:",
+                //     big, i, j, v[i][j]
+                // );
+                big = temp
+            }
+        }
+    });
+    println!("biggest: {}", big);
+}
+
+// What is the value of the first triangle number to have over five hundred divisors?
+#[timings]
+fn e12() {
+    // entire problem is "count divisors". Naive soln sucks. Derive a soln.
+    // Proposition. given X = p_1^a * p_2^b * ...,
+    // N_factors(X) = (a+1)(b+1)....
+    // now we only need to find the algebraic multiplicity of each prime divisor.
+    let multiplicities = |input: usize| -> std::collections::HashMap<usize, usize> {
+        let mut h = std::collections::HashMap::new();
+        let mut n = input;
+        while n % 2 == 0 {
+            let counter = h.entry(2).or_insert(0);
+            *counter += 1;
+            n /= 2;
+        }
+        let mut i = 3;
+        while n > 1 {
+            while n % i == 0 {
+                let counter = h.entry(i).or_insert(0);
+                *counter += 1;
+                n /= i;
+            }
+            i += 2;
+        }
+
+        h
+    };
+
+    let mut i = 1;
+    let mut sum = 0;
+    loop {
+        sum += i;
+        i += 1;
+        let divisors = multiplicities(sum).values().fold(1, |acc, d| acc * (1 + d));
+        //dbg!(sum, divisors);
+        if divisors > 500 {
+            println!("value: {}, the {}th triangle number", sum, i);
+            break;
+        }
+    }
+}
+
 fn main() {
-    let now = std::time::Instant::now();
-    //e11();
-    println!("time:{:?}", now.elapsed());
+    e11();
+    e12();
 }
